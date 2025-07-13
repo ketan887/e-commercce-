792f88a23f1079d94d5a462b702c0b38b6860068
ketan887 <lokhandeketan427@gmail.com>
2025-07-14 00:23:24 +0530

    mongo conneccted

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app= express()
 Port = 3000
 
@@ -7,7 +9,9 @@ app.get('/', (req,res)=>{
     res.send("server is running")
 })
 
-
+  mongoose.connect(process.env.MONGODB_URL)
+  .then(()=> console.log("connected to mongodb"))
+  .catch(err=> console.log(err))
 
 app.listen(Port,()=>{
     console.log("app is running on ${Port}")
